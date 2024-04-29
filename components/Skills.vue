102384e263f1707f78eaaf67885f313f97cc8f16
devAliSalehBH <off.alisaleh@gmail.com>
2024-04-29 14:37:33 +0300

    f6

--- components/Skills.vue
+++ components/Skills.vue
@@ -79,22 +79,22 @@
 <script setup>
 import { ref, onMounted } from "vue";
 const lang = [
-  { name: "C#", path: "/_nuxt/assets/img/img1.png" },
-  { name: "python", path: "/_nuxt/assets/img/img2.png" },
-  { name: "dart", path: "/_nuxt/assets/img/img3.png" },
-  { name: "javaScript", path: "/_nuxt/assets/img/img4.png" },
-  { name: "PHP", path: "/_nuxt/assets/img/img5.png" },
+  { name: "C#", path: "assets/img/img1.png" },
+  { name: "python", path: "assets/img/img2.png" },
+  { name: "dart", path: "assets/img/img3.png" },
+  { name: "javaScript", path: "assets/img/img4.png" },
+  { name: "PHP", path: "assets/img/img5.png" },
 ];
 const frameworks = [
-  { name: "flutter", path: "/_nuxt/assets/img/f-img1.png" },
-  { name: "Vue.js", path: "/_nuxt/assets/img/f-img2.png" },
-  { name: "Laravel", path: "/_nuxt/assets/img/f-img3.png" },
-  { name: "ASP.NET", path: "/_nuxt/assets/img/f-img4.png" },
+  { name: "flutter", path: "assets/img/f-img1.png" },
+  { name: "Vue.js", path: "assets/img/f-img2.png" },
+  { name: "Laravel", path: "assets/img/f-img3.png" },
+  { name: "ASP.NET", path: "assets/img/f-img4.png" },
 ];
 const db = [
-  { name: "SQL", path: "/_nuxt/assets/img/d-img1.png" },
-  { name: "my-SQL", path: "/_nuxt/assets/img/d-img2.png" },
-  { name: "SQLite", path: "/_nuxt/assets/img/d-img3.png" },
+  { name: "SQL", path: "assets/img/d-img1.png" },
+  { name: "my-SQL", path: "assets/img/d-img2.png" },
+  { name: "SQLite", path: "assets/img/d-img3.png" },
 ];
 
 onMounted(() => {
